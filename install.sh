08602499e73c90ed86a7bdad4d5dc36004e69b18
Compro-Prasad <comproprasad@gmail.com>
2018-10-26 14:46:10 +0530

    Generalize installing simple dotfiles in the HOME dir
    Just add new dotfile names to $dotfiles array without the .(dot).

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
 run_dir=$(pwd)
+dotfiles=(
+    Xdefaults
+)
 
-if test -e $HOME/.Xdefaults; then
-    cd $HOME
-    new_name=$(mktemp .Xdefaults.XXXX)
-    mv $HOME/.Xdefaults $new_name
-    echo $HOME/.Xdefaults was moved to $HOME/$new_name
-    cd $run_dir
-fi
-ln -s $(pwd)/.Xdefaults $HOME
+for dotfile in "${dotfiles[@]}"; do
+    dotfile=".$dotfile"
+    echo Installing $dotfile ...
+    src="$run_dir/$dotfile"
+    dst="$HOME/$dotfile"
+    if test -e $dst; then
+        cd $HOME
+        new_name=$(mktemp $dotfile.XXXX)
+        mv $dst $new_name
+        echo $dst was moved to $HOME/$new_name
+        cd $run_dir
+    fi
+    ln -s $src $dst
+    printf -- ' %.0s' {1..10}
+    printf -- '-%.0s' {1..20}
+    echo
+done
